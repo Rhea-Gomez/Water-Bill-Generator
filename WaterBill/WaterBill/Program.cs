16d68609831a845657c096de75a4c2833eecaf91
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-04 23:36:58 +0530

    Assignment on Water Bill Generator

---
+++ WaterBill/WaterBill/Program.cs
@@ -0,0 +1,32 @@
+﻿class WaterBill
+{
+    static int TotalWaterBill(int units)
+    {
+        int meter_charge = 75;
+        int charge;
+        if(units <= 100)
+        {
+            charge = units * 5;
+        }
+        else if(units > 100 && units <= 250)
+        {
+            charge = units * 10;
+        } 
+        else
+        {
+            charge = units * 20;
+        }
+        int total_water_bill = charge + meter_charge;
+        return total_water_bill;
+
+        
+    }
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the no. of units consumed: ");
+        int no_of_units = int.Parse(Console.ReadLine());
+        int bill = TotalWaterBill(no_of_units);
+        Console.Write("Total Water Bill: " +bill);
+        
+    }
+}
